d1f62110351984e9e4bda1d699702818af7c4cac
TOSHIOKANEDA <bhoosama@hotmail.com>
2019-08-12 09:00:24 +0000

    control周りはほど完成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,20 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def edit
+  @user = User.find_by(id: params[:format])
+  end
+  
+  def update
+  user = User.find(params[:id])
+  user.update(user_params)
+  redirect_to "/orders/authority"
+  end
+  
+  private
+  def user_params
+    params.permit(:authority, :email)
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-devise_for :users, controllers: {
-  sessions: 'users/sessions'}
+devise_for :users
   root 'orders#index'
   post "orders/confirm" =>  "orders#confirm"
   post "orders/done" =>  "orders#done"
   get 'orders/authority'  =>  'orders#authority'
+  devise_scope :user do
   get 'users/:id/edit'  =>  'users/sessions#edit'
   patch 'users/:id/edit' => 'users/sessions#update'
-  delete 'users/:id' => 'users/sessions#destroy', as: 'destroy_session_user'
+  delete 'users/:id' => 'users/sessions#destroy'
+  get 'users/:id'  =>  'users/sessions#destroy'
+  end
 end
